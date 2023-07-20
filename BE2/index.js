bc6598dbeb2199b3badad0a14cc4e8b9e6b3d68b
AdeebMisbahi1 <130211733+AdeebMisbahi1@users.noreply.github.com>
2023-07-20 10:03:35 +0530

    access request

--- BE2/index.js
+++ BE2/index.js
@@ -3,6 +3,10 @@ const port=3000;
 
 const app=express();
 
+// how do you access different types of request coming in
+// as simple as that you have to do like this
+//   GET, POST, PUT, PATCH, DELETE <= these are types of request from the user side
+// to full fill these request do this app.get, app.post, app.put, app.patch, app.delete
 app.get('/',function(req, res){
     res.send('<h1>cool, it is running ! is it ?</h1>')
 });
