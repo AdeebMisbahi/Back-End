8f391e36eb39000f9d09a495d412efdb88fabae5
AdeebMisbahi1 <130211733+AdeebMisbahi1@users.noreply.github.com>
2023-07-20 11:20:45 +0530

    Setting Up a template engine

--- BE2/index.js
+++ BE2/index.js
@@ -1,14 +1,14 @@
 const express=require('express');
+const path =require('path');
 const port=3000;
 
 const app=express();
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname, 'views'));
 
-// how do you access different types of request coming in
-// as simple as that you have to do like this
-//   GET, POST, PUT, PATCH, DELETE <= these are types of request from the user side
-// to full fill these request do this app.get, app.post, app.put, app.patch, app.delete
 app.get('/',function(req, res){
-    res.send('<h1>cool, it is running ! is it ?</h1>')
+    // console.log(__dirname);
+  return res.render('home');
 });
 
 app.listen(port, function(err){
