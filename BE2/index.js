dc222f6d17375ce7b5ea18a920b937b64e58947f
AdeebMisbahi1 <130211733+AdeebMisbahi1@users.noreply.github.com>
2023-07-20 08:39:48 +0530

    added get and send by express

--- BE2/index.js
+++ BE2/index.js
@@ -3,6 +3,10 @@ const port=3000;
 
 const app=express();
 
+app.get('/',function(req, res){
+    res.send('<h1>cool, it is running ! is it ?</h1>')
+});
+
 app.listen(port, function(err){
     if (err){
         console.log('Error in running the server',err);
